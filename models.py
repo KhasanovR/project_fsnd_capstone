04e13e6193d65d8fa6862d12f79932aa31f80f55
Rakhmatjon Khasanov <rakhmatjon.khasanov2711@gmail.com>
2020-07-21 11:31:22 +0500

    Add Movie model & Create db init for it

--- models.py
+++ models.py
@@ -1,4 +1,11 @@
 
+from datetime import date
+from sqlalchemy import (
+	Column,
+	String,
+	Integer,
+	Date
+	)
 from flask_sqlalchemy import SQLAlchemy
 from config import database_setup
 
@@ -30,4 +37,47 @@ def db_drop_and_create_all():
     
     db.drop_all()
     db.create_all()
+    db_init()
+
+def db_init():
+
+    new_movie = (Movie(
+        title = 'My Udacity FSND Capstone Movie',
+        release_date = date.today()
+        ))
+	
+	new_movie.insert()
+    
+# ---------------------------------------------------------------------------- #
+# Movies Model 																   #
+# ---------------------------------------------------------------------------- #
+
+class Movie(db.Model):  
+  __tablename__ = 'movies'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release_date = Column(Date)
+  
+  def __init__(self, title, release_date) :
+    self.title = title
+    self.release_date = release_date
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title' : self.title,
+      'release_date': self.release_date
+    }
     
