1917d4aa402064c7b61f3910530ea655045cf0cf
Rakhmatjon Khasanov <rakhmatjon.khasanov2711@gmail.com>
2020-07-22 14:39:45 +0500

    Disable db_drop_and_create_all() temporarily

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ ROWS_PER_PAGE = int(PAGINATION)
 def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
-    db_drop_and_create_all()
+    # db_drop_and_create_all()
 
     CORS(app)
 
